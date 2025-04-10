aece4b538b1d0078938c1e35c22517b94fe6bc5d
Asad Ali <asadjattofficials@gmail.com>
2025-04-10 14:28:05 +0500

    fourth loop

--- index.js
+++ index.js
@@ -7,4 +7,10 @@ for(let i = 1; i < 11; i++){
   console.log('asad');
 }
 //third loop
-
+for(let i = 1; i < 11; i++){
+  console.log('asad');
+}
+//fourth loop
+for(let i = 1; i < 11; i++){
+  console.log('asad');
+}
